9fcb282ccd35f3c46677542a683f9917f6e26c2d
SeunghunChae <48344785+SeunghunChae@users.noreply.github.com>
2023-03-28 17:34:38 +0900

    아웃풋 추가

--- 등급 execute.py
+++ 등급 execute.py
@@ -1,16 +1,17 @@
 lista=[]
-with open('최종_origin.csv','r',newline='') as f:
+with open('최종_execute.csv','r',newline='') as f:
     while True:
         line=f.readline()
         lista.append(line)
         #print(line)
         if len(line)==0:
             break
-
+end=[]
 #lista=lista[0:10]
-with open('최종2_origin.csv','w',newline='') as f:
+with open('최종2_execute.csv','w',newline='') as f:
     for i in lista:
         i=i.replace('\n','')
+        i=i.replace('\r','')
         line=i.split(',')
         if line[0]=='한신평cp':
             if line[6].find('A1')!=-1:
@@ -41,29 +42,29 @@ with open('최종2_origin.csv','w',newline='') as f:
                 i+=',변환등급'
         elif line[0]=='나신평cp':
             if line[5].find('A1')!=-1:
-                i+=',,,,110'
+                i+=',110'
             elif line[5].find('A2+')!=-1:
-                i+=',,,,120'
+                i+=',120'
             elif line[5].find('A2')!=-1:
-                i+=',,,,121'
+                i+=',121'
             elif line[5].find('A2-')!=-1:
-                i+=',,,,122'
+                i+=',122'
             elif line[5].find('A3+')!=-1:
-                i+=',,,,130'
+                i+=',130'
             elif line[5].find('A3')!=-1:
-                i+=',,,,131'
+                i+=',131'
             elif line[5].find('A3-')!=-1:
-                i+=',,,,132'
+                i+=',132'
             elif line[5].find('B+')!=-1:
-                i+=',,,,210'
+                i+=',210'
             elif line[5].find('B')!=-1:
-                i+=',,,,211'
+                i+=',211'
             elif line[5].find('B-')!=-1:
-                i+=',,,,212'
+                i+=',212'
             elif line[5].find('C')!=-1:
-                i+=',,,,310'
+                i+=',310'
             elif line[5].find('D')!=-1:
-                i+=',,,,410'
+                i+=',410'
             else:
                 i+=',변환등급'
         elif line[0]=='한신평stb':
@@ -95,32 +96,33 @@ with open('최종2_origin.csv','w',newline='') as f:
                 i+=',변환등급'
         elif line[0]=='나신평stb':
             if line[5].find('A1')!=-1:
-                i+=',,,,110'
+                i+=',110'
             elif line[5].find('A2+')!=-1:
-                i+=',,,,120'
+                i+=',120'
             elif line[5].find('A2')!=-1:
-                i+=',,,,121'
+                i+=',121'
             elif line[5].find('A2-')!=-1:
-                i+=',,,,122'
+                i+=',122'
             elif line[5].find('A3+')!=-1:
-                i+=',,,,130'
+                i+=',130'
             elif line[5].find('A3')!=-1:
-                i+=',,,,131'
+                i+=',131'
             elif line[5].find('A3-')!=-1:
-                i+=',,,,132'
+                i+=',132'
             elif line[5].find('B+')!=-1:
-                i+=',,,,210'
+                i+=',210'
             elif line[5].find('B')!=-1:
-                i+=',,,,211'
+                i+=',211'
             elif line[5].find('B-')!=-1:
-                i+=',,,,212'
+                i+=',212'
             elif line[5].find('C')!=-1:
-                i+=',,,,310'
+                i+=',310'
             elif line[5].find('D')!=-1:
-                i+=',,,,410'
+                i+=',410'
             else:
                 i+=',변환등급'
-        i+='\n'
+        i+='\n'     
         f.write(i)
+        end.append(i)
     
         
